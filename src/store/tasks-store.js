87777afbc7d54d59a6b393211af03d6a818b4ad6
Damian <damianmirek1234@gmail.com>
2022-10-14 12:56:50 +0200

    Second task is done

---
+++ src/store/tasks-store.js
@@ -0,0 +1,42 @@
+export const ICONS_DESCRIPTION = [
+  {
+    status: "done",
+    description: "This task is done.",
+  },
+  {
+    status: "active",
+    description: "This task is in progress",
+  },
+  {
+    status: "blocked",
+    description: "This task is blocked yet",
+  },
+];
+
+export const DUMMY_TASKS = [
+  {
+    title: "Application Setup",
+    status: ICONS_DESCRIPTION[0].status,
+    businessContexts: [],
+  },
+  {
+    title: "Static Book List",
+    status: ICONS_DESCRIPTION[0].status,
+    businessContexts: [],
+  },
+  {
+    title: "Administration Panel",
+    status: ICONS_DESCRIPTION[0].status,
+    businessContexts: [],
+  },
+  {
+    title: "Connect Admin with Frontend",
+    status: ICONS_DESCRIPTION[1].status,
+    businessContexts: [],
+  },
+  {
+    title: "Book Review Feature",
+    status: ICONS_DESCRIPTION[2].status,
+    businessContexts: [],
+  },
+];
