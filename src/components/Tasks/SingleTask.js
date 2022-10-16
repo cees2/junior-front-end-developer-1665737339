9a3fedfa0099394910f9d4c45d0fb0963368cedf
Damian <damianmirek1234@gmail.com>
2022-10-16 19:55:51 +0200

    Task 2 is done

--- src/components/Tasks/SingleTask.js
+++ src/components/Tasks/SingleTask.js
@@ -1,11 +1,22 @@
 import classes from "./SingleTask.module.css";
 import Arrow from "../../images/Arrow.svg";
+import Checkmark from "../../images/Checkmark.svg";
+import Locker from "../../images/Locker.svg";
+import { ICONS_DESCRIPTION } from "../../store/tasks-store";
 
-const SingleTask = () => {
+const ICONS = {
+  done: Checkmark,
+  active: Arrow,
+  blocked: Locker,
+};
+
+const SingleTask = (props) => {
+  const { status } = props;
+  const statusCode = status === "done" ? 0 : status === "active" ? 1 : 2;
   return (
-    <li>
-      <img src={Arrow} alt="Arrow" />
-      Connect Admin With Frontend
+    <li className={classes.taskItem}>
+      <img src={ICONS[status]} alt={ICONS_DESCRIPTION[statusCode].alt} />
+      {props.taskTitle}
     </li>
   );
 };
