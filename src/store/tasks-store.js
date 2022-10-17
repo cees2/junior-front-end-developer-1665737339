9338d12e8fcaa6460d3c9d80cf6442a06b82987b
cees2 <damianmirek1234@gmail.com>
2022-10-17 19:58:53 +0200

    Improved App Layout

--- src/store/tasks-store.js
+++ src/store/tasks-store.js
@@ -49,6 +49,96 @@ const DUMMY_TASKS = [
         ),
         isNew: false,
       },
+      {
+        title: "New sprint, tasks and intro information",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget luctus nisi. Morbi ut sem suscipit, aliquet turpis a, vehicula massa. Aliquam a viverra metus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer tincidunt ultrices feugiat. Nunc vel bibendum lacus, ac gravida massa. Sed pharetra luctus erat in malesuada. Nulla rhoncus sem sed nulla euismod tincidunt. Curabitur sollicitudin a nisl non malesuada. Morbi tempus risus nisi, nec eleifend sapien fringilla vel. Nam sed ipsum porttitor, sollicitudin justo at, convallis odio. Integer viverra maximus arcu quis bibendum. Nunc magna odio, posuere quis semper vitae, mollis ac sapien.",
+        author: "Project Manager",
+        createdAt: new Date(
+          Date.now() - Math.round(Math.random() * 24 * 60 * 60 * 1000)
+        ),
+        isNew: true,
+      },
+      {
+        title: "Your task",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget luctus nisi. Morbi ut sem suscipit, aliquet turpis a, vehicula massa. Aliquam a viverra metus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer tincidunt ultrices feugiat. Nunc vel bibendum lacus, ac gravida massa. Sed pharetra luctus erat in malesuada. Nulla rhoncus sem sed nulla euismod tincidunt. Curabitur sollicitudin a nisl non malesuada. Morbi tempus risus nisi, nec eleifend sapien fringilla vel. Nam sed ipsum porttitor, sollicitudin justo at, convallis odio. Integer viverra maximus arcu quis bibendum. Nunc magna odio, posuere quis semper vitae, mollis ac sapien.",
+        author: "Kristen Aniston",
+        createdAt: new Date(
+          Date.now() - Math.round(Math.random() * 20 * 60 * 60 * 1000)
+        ),
+        isNew: false,
+      },
+      {
+        title: "Holidays plans",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget luctus nisi. Morbi ut sem suscipit, aliquet turpis a, vehicula massa. Aliquam a viverra metus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer tincidunt ultrices feugiat. Nunc vel bibendum lacus, ac gravida massa. Sed pharetra luctus erat in malesuada. Nulla rhoncus sem sed nulla euismod tincidunt. Curabitur sollicitudin a nisl non malesuada. Morbi tempus risus nisi, nec eleifend sapien fringilla vel. Nam sed ipsum porttitor, sollicitudin justo at, convallis odio. Integer viverra maximus arcu quis bibendum. Nunc magna odio, posuere quis semper vitae, mollis ac sapien.",
+        author: "Damian Mirek",
+        createdAt: new Date(
+          Date.now() - Math.round(Math.random() * 20 * 60 * 60 * 1000)
+        ),
+        isNew: false,
+      },
+      {
+        title: "New sprint, tasks and intro information",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget luctus nisi. Morbi ut sem suscipit, aliquet turpis a, vehicula massa. Aliquam a viverra metus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer tincidunt ultrices feugiat. Nunc vel bibendum lacus, ac gravida massa. Sed pharetra luctus erat in malesuada. Nulla rhoncus sem sed nulla euismod tincidunt. Curabitur sollicitudin a nisl non malesuada. Morbi tempus risus nisi, nec eleifend sapien fringilla vel. Nam sed ipsum porttitor, sollicitudin justo at, convallis odio. Integer viverra maximus arcu quis bibendum. Nunc magna odio, posuere quis semper vitae, mollis ac sapien.",
+        author: "Project Manager",
+        createdAt: new Date(
+          Date.now() - Math.round(Math.random() * 24 * 60 * 60 * 1000)
+        ),
+        isNew: true,
+      },
+      {
+        title: "Your task",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget luctus nisi. Morbi ut sem suscipit, aliquet turpis a, vehicula massa. Aliquam a viverra metus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer tincidunt ultrices feugiat. Nunc vel bibendum lacus, ac gravida massa. Sed pharetra luctus erat in malesuada. Nulla rhoncus sem sed nulla euismod tincidunt. Curabitur sollicitudin a nisl non malesuada. Morbi tempus risus nisi, nec eleifend sapien fringilla vel. Nam sed ipsum porttitor, sollicitudin justo at, convallis odio. Integer viverra maximus arcu quis bibendum. Nunc magna odio, posuere quis semper vitae, mollis ac sapien.",
+        author: "Kristen Aniston",
+        createdAt: new Date(
+          Date.now() - Math.round(Math.random() * 20 * 60 * 60 * 1000)
+        ),
+        isNew: false,
+      },
+      {
+        title: "Holidays plans",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget luctus nisi. Morbi ut sem suscipit, aliquet turpis a, vehicula massa. Aliquam a viverra metus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer tincidunt ultrices feugiat. Nunc vel bibendum lacus, ac gravida massa. Sed pharetra luctus erat in malesuada. Nulla rhoncus sem sed nulla euismod tincidunt. Curabitur sollicitudin a nisl non malesuada. Morbi tempus risus nisi, nec eleifend sapien fringilla vel. Nam sed ipsum porttitor, sollicitudin justo at, convallis odio. Integer viverra maximus arcu quis bibendum. Nunc magna odio, posuere quis semper vitae, mollis ac sapien.",
+        author: "Damian Mirek",
+        createdAt: new Date(
+          Date.now() - Math.round(Math.random() * 20 * 60 * 60 * 1000)
+        ),
+        isNew: false,
+      },
+      {
+        title: "New sprint, tasks and intro information",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget luctus nisi. Morbi ut sem suscipit, aliquet turpis a, vehicula massa. Aliquam a viverra metus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer tincidunt ultrices feugiat. Nunc vel bibendum lacus, ac gravida massa. Sed pharetra luctus erat in malesuada. Nulla rhoncus sem sed nulla euismod tincidunt. Curabitur sollicitudin a nisl non malesuada. Morbi tempus risus nisi, nec eleifend sapien fringilla vel. Nam sed ipsum porttitor, sollicitudin justo at, convallis odio. Integer viverra maximus arcu quis bibendum. Nunc magna odio, posuere quis semper vitae, mollis ac sapien.",
+        author: "Project Manager",
+        createdAt: new Date(
+          Date.now() - Math.round(Math.random() * 24 * 60 * 60 * 1000)
+        ),
+        isNew: true,
+      },
+      {
+        title: "Your task",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget luctus nisi. Morbi ut sem suscipit, aliquet turpis a, vehicula massa. Aliquam a viverra metus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer tincidunt ultrices feugiat. Nunc vel bibendum lacus, ac gravida massa. Sed pharetra luctus erat in malesuada. Nulla rhoncus sem sed nulla euismod tincidunt. Curabitur sollicitudin a nisl non malesuada. Morbi tempus risus nisi, nec eleifend sapien fringilla vel. Nam sed ipsum porttitor, sollicitudin justo at, convallis odio. Integer viverra maximus arcu quis bibendum. Nunc magna odio, posuere quis semper vitae, mollis ac sapien.",
+        author: "Kristen Aniston",
+        createdAt: new Date(
+          Date.now() - Math.round(Math.random() * 20 * 60 * 60 * 1000)
+        ),
+        isNew: false,
+      },
+      {
+        title: "Holidays plans",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget luctus nisi. Morbi ut sem suscipit, aliquet turpis a, vehicula massa. Aliquam a viverra metus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer tincidunt ultrices feugiat. Nunc vel bibendum lacus, ac gravida massa. Sed pharetra luctus erat in malesuada. Nulla rhoncus sem sed nulla euismod tincidunt. Curabitur sollicitudin a nisl non malesuada. Morbi tempus risus nisi, nec eleifend sapien fringilla vel. Nam sed ipsum porttitor, sollicitudin justo at, convallis odio. Integer viverra maximus arcu quis bibendum. Nunc magna odio, posuere quis semper vitae, mollis ac sapien.",
+        author: "Damian Mirek",
+        createdAt: new Date(
+          Date.now() - Math.round(Math.random() * 20 * 60 * 60 * 1000)
+        ),
+        isNew: false,
+      },
     ],
   },
   {
